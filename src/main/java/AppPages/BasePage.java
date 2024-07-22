73e92a1a288b59bafc548db2719c6bb704b5a353
sharmi07 <sharmidha@yahoo.com>
2024-07-22 11:32:40 -0700

    Update BasePage.java

--- src/main/java/AppPages/BasePage.java
+++ src/main/java/AppPages/BasePage.java
@@ -4,13 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
-    //protected WebDriver driver;
-    // GlobalStore GS = new GlobalStore();
     public BasePage(WebDriver driver) {
-        //this.driver = driver;
-        //  GS.reportStep("In BasePage class initializing PageFactory....., ", "INFO");
         PageFactory.initElements(driver, this);
-        //  GS.reportStep("Initialization Done.", "INFO");
     }
 
 }
